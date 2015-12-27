af60c5bb4488fac19465a0e66ecab9cb59ef407c
Paul Rios <paulocr2@gmail.com>
2015-12-27 19:40:49 -0300

    Done - Final Release

--- server.R
+++ server.R
@@ -1,6 +1,6 @@
 
 library(shiny)
-devtools::install_github('rstudio/shinyapps')
+
 
 coronaryDiseaseRisk <- function(badCholesterol, physicalActivity, 
                                 bloodpressure) {
@@ -11,16 +11,16 @@ coronaryDiseaseRisk <- function(badCholesterol, physicalActivity,
     } else if (badCholesterol < 120 & physicalActivity == 2) {
             risk <- 40
     } else if (badCholesterol > 120 & bloodpressure == 1) {
-        risk <- 80
+            risk <- 80
     } else if (badCholesterol < 120 & bloodpressure == 0 & 
                physicalActivity == 1) {
-        risk <- 20
+            risk <- 20
     } else if (physicalActivity == 3)
-        risk <- 55
+            risk <- 55
     else if (bloodpressure == 1)
-        risk <- 60
+            risk <- 60
     else 
-        risk <- 50
+            risk <- 50
     
     return (risk)
     
@@ -29,11 +29,7 @@ coronaryDiseaseRisk <- function(badCholesterol, physicalActivity,
 
 shinyServer(
     function(input, output) {
-        
-        output$badCholesterol <- renderPrint({input$badCholesterol})
-        output$physicalActivity <- renderPrint({input$physicalActivity})
-        output$bloodPressure <- renderPrint({input$bloodPressure})
-        
+
         output$prediction <- renderPrint({coronaryDiseaseRisk(input$badCholesterol,
                                                         input$physicalActivity,
                                                         input$bloodpressure)})
